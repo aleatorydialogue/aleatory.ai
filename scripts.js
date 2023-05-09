eec102cfce14ec596cedae3b653d67549d640eea
Timothy Tulloch <aleatorydialogue@gmail.com>
2023-05-09 19:05:24 -0400

    reorganizing initial scope

--- scripts.js
+++ scripts.js
@@ -1,12 +1,20 @@
 document.addEventListener('DOMContentLoaded', function () {
   var images = [
+    '1.png',
     '2.png',
-    'ad.mp3',
-    'bubbles.png',
-    'city.png',
-    'dead.png',
-    'portal.png',
-    'skygarden.png',
+    '3.png',
+    '4.png',
+    '5.png',
+    '6.png',
+    '7.png',
+    '8.png',
+    '9.png',
+    '10.png',
+    '11.png',
+    '12.png',
+    '13.png',
+    '14.png',
+    '15.png',
     // Add more image file names here
   ];
 
