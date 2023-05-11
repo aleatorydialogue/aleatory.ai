e2b07b0a0106f23fdf69cd347ba44a8d0037d5d5
Timothy Tulloch <aleatorydialogue@gmail.com>
2023-05-11 12:27:29 -0400

    more background images

--- scripts.js
+++ scripts.js
@@ -14,7 +14,32 @@ document.addEventListener('DOMContentLoaded', function () {
     'backgrounds/12.png',
     'backgrounds/13.png',
     'backgrounds/14.png',
-    'backgrounds/15.png'
+    'backgrounds/15.png',
+    'backgrounds/16.png',
+    'backgrounds/17.png',
+    'backgrounds/18.png',
+    'backgrounds/19.png',
+    'backgrounds/20.png',
+    'backgrounds/21.png',
+    'backgrounds/22.png',
+    'backgrounds/23.png',
+    'backgrounds/24.png',
+    'backgrounds/25.png',
+    'backgrounds/26.png',
+    'backgrounds/27.png',
+    'backgrounds/28.png',
+    'backgrounds/29.png',
+    'backgrounds/30.png',
+    'backgrounds/31.png',
+    'backgrounds/32.png',
+    'backgrounds/33.png',
+    'backgrounds/34.png',
+    'backgrounds/35.png',
+    'backgrounds/36.png',
+    'backgrounds/37.png',
+    'backgrounds/38.png',
+    'backgrounds/39.png',
+    'backgrounds/40.png'
     // Add more image file names here
   ];
 
