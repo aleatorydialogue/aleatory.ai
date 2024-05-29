3831fe50a9467dfb506e22e2e50536a18c6769e6
Timothy Tulloch <aleatorydialogue@gmail.com>
2024-05-29 15:59:14 -0400

    attempting fix on dynamic splat model selection 3

--- hybrid.js
+++ hybrid.js
@@ -1002,7 +1002,7 @@ async function main() {
     }
   };
 
-  const url = params.get("url") ? new URL(params.get("url"), "https://huggingface.co/aleatorydialogue/dynamic_splats/tree/main/") : "god.splatv";
+  const url = params.get("url") ? new URL(params.get("url"), "https://huggingface.co/aleatorydialogue/dynamic_splats/resolve/main/") : "timer.splatv";
   const req = await fetch(url, { mode: "cors", credentials: "omit" });
   if (req.status != 200) throw new Error(req.status + " Unable to load " + req.url);
 
